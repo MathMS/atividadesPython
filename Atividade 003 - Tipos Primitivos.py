1282c7b6c421dc5165b46291e778ead0f0b28cae
MathMS <mathdts@gmail.com>
2018-11-08 15:38:06 -0200

    Melhor sintaxe

--- Atividade 003 - Tipos Primitivos.py
+++ Atividade 003 - Tipos Primitivos.py
@@ -1,7 +1,7 @@
 
 """Crie um programa que receba dois números digitados pelo usuário e depois mostre a soma"""
 
-n1 = input('\nDigite o primeiro número: ')
-n2 = input('Digite o segundo número: ')
-soma = int(n1) + int(n2)
-print('A soma dos dois números deu: ', soma)
+n1 = int(input('\nDigite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print('A soma dos dois números vale: ', soma)
