561c5dee54f9a420130207c5fb6d527d115f8044
MathMS <mathdts@gmail.com>
2019-01-14 19:37:50 -0200

    Listas pt. 2

--- Mundo 03 - Estruturas Compostas/Atividade 085 - Listas.py
+++ Mundo 03 - Estruturas Compostas/Atividade 085 - Listas.py
@@ -1,14 +1,16 @@
 '''Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados
 os valores paras e ímpares. No final mostre os valores pares e ímpares em ordem crescente.'''
 
-pares = []
-impares = []
-lista = []
-
-for n in range(0, 7):
-    numero = int(input(f'Digite o {n+1}° número: '))
-    if numero%2 == 0:
-        pares.append(numero)
+num = [[], []]
+valor = 0
+for c in range(1,8):
+    valor = int(input(f'Digite o {c}° valor: '))
+    if valor%2 == 0:
+        num[0].append(valor)
     else:
-        impares.append(numero)
+        num[1].append(valor)
+num[0].sort()
+num[1].sort()
+print(f'Os valore pares digitados foram: {num[0]}')
+print(f'Os valores ímpares digitados foram: {num[1]}')
 
